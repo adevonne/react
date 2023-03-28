147bd944374ed7fb794254c878440f2d7c83ece3
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-03-28 17:04:11 +0200

    ajout mew

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,10 +28,10 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
         <PokemonCard/>
     </div>
-  ); 
+  )
 }
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,33 @@
 import React from "react"; 
 
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",  
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
+  const pokemon = pokemonList[1]; 
+  
   return (
-  <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-    <figcaption>bulbasaur</figcaption>
+    <figure>
+      {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} />  : "???" }
+    <figcaption>{pokemon.name}</figcaption>
   </figure>
+  
   ); 
+
+
+   
 }
+
+
+
+
 
 export default PokemonCard;

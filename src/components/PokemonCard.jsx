a2a79563ca798777e7f651f6c038d5408df79add
Ribeiro Jessica <ribeirofj@yahoo.com>
2023-03-29 12:21:36 +0200

    ajout props

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,33 +1,13 @@
 import React from "react"; 
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",  
-  },
-  {
-    name: "mew",
-  },
-];
-
-function PokemonCard() {
-  const pokemon = pokemonList[1]; 
-  
+function PokemonCard(props) {
+  const {pokemon} = props
   return (
     <figure>
       {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt={pokemon.name} />  : "???" }
     <figcaption>{pokemon.name}</figcaption>
   </figure>
-  
   ); 
-
-
-   
 }
-
-
-
-
 
 export default PokemonCard;
